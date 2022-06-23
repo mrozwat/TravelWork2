597f06534b295edf962733af5e30d218b8d35fb1
Sergey Andreev <ozwatandree@gmail.com>
2022-06-24 02:14:35 +0300

    2

--- webpack.config.js
+++ webpack.config.js
@@ -10,5 +10,17 @@ module.exports = {
     devServer: {
         hot: false
       },
-  
+      module: {
+        rules: [
+            {
+              test: /\.js$/,
+              exclude: /(node_modules)/,
+              use: ['babel-loader']
+            },
+            {
+              test: /\.css$/i,
+              use: ['style-loader', 'css-loader']
+            }
+        ]
+      }
 }
