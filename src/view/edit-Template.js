41157d0a657cbdbd6d266b7576f5479c076ad49f
mrozwat <80839365+mrozwat@users.noreply.github.com>
2022-06-26 21:07:57 +0300

    EditTempl Add photo

--- src/view/edit-Template.js
+++ src/view/edit-Template.js
@@ -1,6 +1,7 @@
 import   {testData}from'../mock/test-data.js';
 import {offerType} from './edit-emplate-offers-types.js'
 import { offerListToEdit } from './edit-template-offers-list.js';
+import {editPhoto} from './edit-photo.js'
 const dayjs = require('dayjs');
 
 function edit (){
@@ -65,7 +66,12 @@ function edit (){
   <section class="event__section  event__section--destination">
     <h3 class="event__section-title  event__section-title--destination">Destination</h3>
     <p class="event__destination-description">${testData[i].destination.description}</p>
-  </section>
+    <div class="event__photos-container">
+    <div class="event__photos-tape">
+      ${editPhoto(i)}
+    </div>
+  </div>
+    </section>
 </section>
 </form>`;
 return editHtml;
