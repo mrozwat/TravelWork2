88c6ba57dff7ea2cfa48fc1f55188de74608ddc2
Sergey Andreev <ozwatandree@gmail.com>
2022-07-14 03:09:44 +0300

    add filter

---
+++ src/presenter/filter-presenter.js
@@ -0,0 +1,67 @@
+import filtersElement from '../view/filters-Template';
+import { RenderPosition, remove, renderElement} from '../render/render';
+import {FilterType, UpdateType} from '../util/util';
+import { replace } from '../util/util.js';
+
+export default class FilterPresenter {
+  #filterContainer = null;
+  #filterModel = null;
+  #pointModel = null;
+
+  #filterComponent = null;
+
+  constructor(filterContainer, filterModel, pointModel) {
+    this.#filterContainer = filterContainer;
+    this.#filterModel = filterModel;
+    this.#pointModel = pointModel;
+
+    this.#pointModel.addObserver(this.#handleModelEvent);
+    this.#filterModel.addObserver(this.#handleModelEvent);
+  }
+
+  get filters() {
+
+    return [
+      {
+        type: FilterType.ALL,
+        name: 'All',
+      },
+      {
+        type: FilterType.FUTURE,
+        name: 'future',
+      },
+      {
+        type: FilterType.PAST,
+        name: 'past',
+      }
+    ];
+  }
+
+  init = () => {
+    const filters = this.filters;
+    const prevFilterComponent = this.#filterComponent;
+
+    this.#filterComponent = new filtersElement(filters, this.#filterModel.filter);
+    this.#filterComponent.setFilterTypeChangeHandler(this.#handleFilterTypeChange);
+
+    if (prevFilterComponent === null) {
+      renderElement(this.#filterContainer, this.#filterComponent, RenderPosition.BEFOREEND);
+      return;
+    }
+
+    replace(this.#filterComponent, prevFilterComponent);
+    remove(prevFilterComponent);
+  }
+
+  #handleModelEvent = () => {
+    this.init();
+  }
+
+  #handleFilterTypeChange = (filterType) => {
+    if (this.#filterModel.filter === filterType) {
+      return;
+    }
+
+    this.#filterModel.setFilter(UpdateType.MAJOR, filterType);
+  }
+}
