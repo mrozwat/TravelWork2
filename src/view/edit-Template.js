2f6bd96b85cfbacf67fd53152559b35b26ffbbf3
Sergey Andreev <ozwatandree@gmail.com>
2022-07-19 01:06:49 +0300

    delaet console.log

--- src/view/edit-Template.js
+++ src/view/edit-Template.js
@@ -237,7 +237,6 @@ export default  class editElement extends AbstractSmartView{
   });
   delete this._dataCondition.timeFrom;
   delete this._dataCondition.timeTo;
-  console.log(this._dataCondition);
 
 }
 
